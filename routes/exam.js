e601ec1029ee4e80800a7ecb021df7242a869b60
Oliver Lonsdale <lonsdale1086@outlook.com>
2022-04-08 01:52:17 +0100

    tweaks to saving of exams

--- routes/exam.js
+++ routes/exam.js
@@ -16,10 +16,15 @@ router.get('/view/:examID', ensureAuthenticated, async (req, res) => {
     return
   }
   try {
-    const exam = await Exam.findOne({ _id: id })
+    const exam = await Exam.findOne({
+      _id: id
+    })
     const optoms = await User.find()
-    if (exam) { 
-      res.render('exam-view', { exam, optoms })
+    if (exam) {
+      res.render('exam-view', {
+        exam,
+        optoms
+      })
       return
     }
     throw ("not found")
@@ -36,8 +41,12 @@ router.get('/view', ensureAuthenticated, (req, res) => {
 router.get('/add/:patientID', ensureAuthenticated, async (req, res) => {
   const id = req.params.patientID
   try {
-    const patient = await Patient.findOne({ _id: id })
-    const optometrists = await User.find({ optometrist: true })
+    const patient = await Patient.findOne({
+      _id: id
+    })
+    const optometrists = await User.find({
+      optometrist: true
+    })
     if (patient) {
       res.render('exam-add', {
         user: req.user,
@@ -52,10 +61,12 @@ router.get('/add/:patientID', ensureAuthenticated, async (req, res) => {
   }
 })
 
-router.get('/add', ensureAuthenticated, (req, res) => {res.render('errors/404');})
+router.get('/add', ensureAuthenticated, (req, res) => {
+  res.render('errors/404');
+})
 
 router.post('/add', ensureAuthenticated, async (req, res) => {
-  console.log(req.body)
+  console.log("request body: \n", req.body, "\n\n")
 
   let exam = {
     patientID: req.body.patientID,
@@ -71,7 +82,6 @@ router.post('/add', ensureAuthenticated, async (req, res) => {
     performingOptometrist: req.body.optometrist,
     author: req.body.author,
     //date added auto
-
     lensOpacityLeft: req.body.lensOpacityLeft,
     lensOpacityRight: req.body.lensOpacityRight,
     lensComment: req.body.lensComment,
@@ -95,49 +105,71 @@ router.post('/add', ensureAuthenticated, async (req, res) => {
     convergence: req.body.convergence,
     stereopsis: req.body.stereopsis,
     //va
-    sphereLeft: `${req.body.sphereLeftSign}${req.body.sphereLeft}`,
-    sphereRight: `${req.body.sphereRighttSign}${req.body.sphereRightt}`,
+    //sphere
     cylinderLeft: req.body.cylinderLeft,
     cylinderRight: req.body.cylinderRight,
     axisLeft: req.body.axisLeft,
     axisRight: req.body.axisRight,
-    prismLeft: req.body.prismLeft,
-    prismRight: req.body.prismRight,
-    adviceGiven: `${req.body.adviceGiven} - ${req.body.adviceSpecifics}`,
+    //prism
     recallCode: req.body.recallCode,
   }
+  exam.adviceGiven = `${req.body.adviceGiven}`
+  if (req.body.adviceSpecifics) exam.adviceGiven = `${req.body.adviceGiven} - ${req.body.adviceSpecifics}`
 
-  if(req.body.patientDrives){exam.drives = true}
-  if(req.body.patientUsesVDU){exam.vdu = true}
+  if (req.body.patientDrives) {
+    exam.patientDrives = true
+  } else exam.patientDrives = false
 
-  if(req.body.fundusLeft == "other"){
+  if (req.body.patientUsesVDU) {
+    exam.patientUsesVDU = true
+  } else exam.patientUsesVDU = false
+
+  if (req.body.fundusLeft == "other") {
     exam.fundusLeft = req.body.fundusLeftOther
   } else exam.fundusLeft = req.body.fundusLeft
-  
-  if(req.body.fundusRight == "other"){
+
+  if (req.body.fundusRight == "other") {
     exam.fundusRight = req.body.fundusRightOther
   } else exam.fundusRight = req.body.fundusRight
-  
-  if(req.body.maculaLeft == "other"){
+
+  if (req.body.maculaLeft == "other") {
     exam.maculaLeft = req.body.maculaLeftOther
   } else exam.maculaLeft = req.body.maculaLeft
 
-  if(req.body.maculaRight == "other"){
+  if (req.body.maculaRight == "other") {
     exam.maculaRight = req.body.maculaRightOther
   } else exam.maculaRight = req.body.maculaRight
 
-  if(req.body.motility == "other"){
+  if (req.body.motility == "other") {
     exam.motility = req.body.motilityOther
   } else exam.motility = req.body.motility
 
+  if (req.body.visualAcuityMod == 0) {
+    exam.visualAcuity = req.body.visualAcuity
+  } else exam.visualAcuity = `${req.body.visualAcuity} ${req.body.visualAcuityMod}`
 
-  if(req.body.visualAcuityMod == 0){
-    exam.visualAcuity = `${req.body.visualAcuity} ${req.body.visualAcuityMod}`
-  } else exam.visualAcuity = req.body.visualAcuity
+  if (Array.isArray(req.body.prismLeft)) {
+    exam.prismBaseLeft = req.body.prismLeft.join()
+  } else exam.prismBaseLeft = req.body.prismLeft
+
+  if (Array.isArray(req.body.prismRight)) {
+    exam.prismBaseRight = req.body.prismRight.join()
+  } else exam.prismBaseRight = req.body.prismRight
+
+  if (req.body.sphereLeft != "plano") {
+    exam.sphereLeft = `${req.body.sphereLeftSign}${req.body.sphereLeft}`
+  } else exam.sphereLeft = `${req.body.sphereLeft}`
+
+  if (req.body.sphereRight != "plano") {
+    exam.sphereRight = `${req.body.sphereRightSign}${req.body.sphereRight}`
+  } else exam.sphereRight = `${req.body.sphereRight}`
+
 
   const newExam = new Exam(exam)
 
   newExam.save()
+
+  console.log("Saved exam: \n", newExam, "\n\n")
 
   res.redirect(`/patient/view/${req.body.patientID}`)
 });
