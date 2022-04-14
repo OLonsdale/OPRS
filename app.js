9355dd361edbdbd57a6ff69dec921f090a4e9cdd
Oliver Lonsdale <lonsdale1086@outlook.com>
2022-04-14 23:59:53 +0100

    fixed styles, broke add-patient, working on get req

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const morgan = require('morgan');
-const path = require("path");
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const flash = require('connect-flash')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const morgan = require('morgan')
+const path = require("path")
 
-const app = express();
+const app = express()
+app.use(express.static(path.join(__dirname,"public")));
 
 // Logging when dev mode enabled
 app.use(morgan("dev"))
 
 
 // Passport Config
-require('./config/passport')(passport);
+require('./config/passport')(passport)
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI
 
 // Connect to MongoDB
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, )
   .then(() => console.log(`MongoDB connected`))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
 // EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
 
 // Express body parser
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // Express session
 app.use(
@@ -40,32 +41,30 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({mongoUrl: db}),
   }),
-);
+)
 
 // Passport middleware for varification
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Connect flash, for passing messeges.
-app.use(flash());
+app.use(flash())
 
 // Global variables
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
 });
 
-//static folder for assets
-app.use(express.static(path.join(__dirname,"public")))
 
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/patient', require('./routes/patient'));
-app.use('/exam', require('./routes/exam'));
-app.use('/staff', require('./routes/staff'));
+app.use('/', require('./routes/index'))
+app.use('/patient', require('./routes/patient'))
+app.use('/exam', require('./routes/exam'))
+app.use('/staff', require('./routes/staff'))
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5500
 
-app.listen(PORT, console.log(`Server running on ${PORT}`));
+app.listen(PORT, console.log(`Server running on ${PORT}`))
